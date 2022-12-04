0452ccf583e98e1db06de85ab565008df42da59a
Giles <gilesnp@gmail.com>
2022-12-04 15:11:33 +0000

    Spacing

--- resources/views/wonde/employee.blade.php
+++ resources/views/wonde/employee.blade.php
@@ -7,14 +7,18 @@
     <p class="subtitle">Hello <strong>{{ $employee->title }} {{ $employee->forename }} {{ $employee->surname }}</strong>.</p>
     <p>Please select a day to view:</p>
     <form action="/wonde/school/employee/classesForDay" method="POST" enctype="multipart/form-data">
+
         @csrf
+        
         <div class="select">
             <select name="day_title">
-            
+                
                 @foreach ($days as $key => $day)
+
                     <option value="{{ $key }}">
                         {{ ucfirst($key) }}
                     </option>
+
                 @endforeach
                 
             </select>
